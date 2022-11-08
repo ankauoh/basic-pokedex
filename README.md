2c2f542330f10f4ad5232ccfab4d1f918cb2611b
ankauoh <andreas.kauderer@protonmail.com>
2022-11-08 18:28:11 +0100

    README.md initial input, js  minor syntax changes

---
+++ README.md
@@ -0,0 +1,9 @@
+Description: This project is a simple pokedex to get into the basics of javascript.
+
+Built with:
+  HTML
+  CSS
+  Javascript
+  VSCode
+
+  To Run Locally: Download Repository off of Github, and open index.html
--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
   { name: 'Bulbasaur', height: 4, type: ['grass', 'poison'] },
-  { name: 'Pikachu', height: 3, type: ['electric]'},
+  { name: 'Pikachu', height: 3, type: ['electric'] },
   { name: 'Charmander', height: 4, type: ['fire'] }
 ];
