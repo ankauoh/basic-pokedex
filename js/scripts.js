90c6318e5b4cf7f1285d0b7acff7bb05c9e8a2bf
ankauoh <andreas.kauderer@protonmail.com>
2022-11-08 17:55:58 +0100

    js/ array added
    first array with 3 pokemon objects added

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-alert('Hello world');
-
-var favoriteFood = 'Ramen';
-document.write(favoriteFood);
+let pokemonList = [
+  { name: 'Bulbasaur', height: 4, type: ['grass', 'poison'] },
+  { name: 'Pikachu', height: 3, type: ['electric]'},
+  { name: 'Charmander', height: 4, type: ['fire'] }
+];
