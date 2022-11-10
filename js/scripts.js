bc2cfcdf09ab2813fb0df081386ae11b4fce4733
ankauoh <andreas.kauderer@protonmail.com>
2022-11-10 18:24:59 +0100

    CSS changes; js formatting changes
    CSS: logo formatted with max-height
    
    js: loop limited by .length, rather than a number. <p> and </p> tags included in loop.

--- js/scripts.js
+++ js/scripts.js
@@ -18,13 +18,16 @@ let pokemonList = [
 //The code below runs a loop to list the names and heights of the pokemon
 // VSCode recommended using template literals instead of the quotes I was using
 
-for (let i = 0; i < 3; i++) {
-  if (pokemonList[i].height <= 3)
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}), `);
-  else {
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!, `);
-  }
-  //Add <p> so each iteration starts a new line
+for (let i = 0; i < pokemonList.length; i++) {
+  //opening <p> tag, so the iteration is formatted in html
   document.write('<p>');
+
+  if (pokemonList[i].height <= 3)
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `);
+  else {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! `);
+  }
+  //closing </p> tag so the iteration is html valid
+  document.write('</p>');
 
 }
