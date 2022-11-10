09337a0dd3a63b2b710ef5a64ba11c08b1484d1e
ankauoh <andreas.kauderer@protonmail.com>
2022-11-10 13:32:16 +0100

    css update/ js loop introduced
    CSS: began formatting the stylesheet based off of an old sheet. Only logo and font are currently affected
    
    js: a loop with a height condition used for listing pokemone array
    
    index: link to logo added

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
 let pokemonList = [
-  { name: 'Bulbasaur', height: 4, type: ['grass', 'poison'] },
-  { name: 'Pikachu', height: 3, type: ['electric'] },
-  { name: 'Charmander', height: 4, type: ['fire'] }
+  {
+    name: 'Bulbasaur',
+    height: 4,
+    type: ['grass', 'poison']
+  },
+  {
+    name: 'Pikachu',
+    height: 3,
+    type: ['electric']
+  },
+  {
+    name: 'Charmander',
+    height: 4,
+    type: ['fire']
+  }
 ];
+//The code below runs a loop to list the names and heights of the pokemon
+// VSCode recommended using template literals instead of the quotes I was using
+
+for (let i = 0; i < 3; i++) {
+  if (pokemonList[i].height <= 3)
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}, `);
+  else {
+    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!, `);
+  }
+}
