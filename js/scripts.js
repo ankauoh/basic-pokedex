74424f79c34045e7ee974458bd0dd0674890d8c5
ankauoh <andreas.kauderer@protonmail.com>
2022-11-16 13:57:00 +0100

    CSS button styl; index <ul> addition; js pokemon loop changed
    CSS - primary button styling added
    
    Index - <ul> added in body to add structure to the js pokemon loop
    
    js - forEach function blcok moved into IIFE. function block reformatted using queryselectot and actions on the button class. Eventlistener added into addListItem. eventlistener calls new function (showDetails)

--- js/scripts.js
+++ js/scripts.js
@@ -25,39 +25,32 @@ let pokemonRepository = (function () {
   function getAll() {
     return pokemonList;
   }
+  //addListItem function for listing pokemon details
+  function addListItem(pokemon) {
+    let pokemonList = document.querySelector('.pokemon-list');
+    let listPokemon = document.createElement('li');
+    let button = document.createElement('button');
+    button.innerText = pokemon.name;
+    button.classList.add("primary-button");
+    listPokemon.appendChild(button);
+    pokemonList.appendChild(listPokemon);
+    button.addEventListener('click', function (ev) {
+      showDetails(pokemon); //calls the function below
+    });
+  }
+  //function referenced above to console log the details
+  function showDetails(pokemon) {
+    console.log(pokemon);
+  }
 
   return {
     add: add,
-    getAll: getAll
+    getAll: getAll,
+    addListItem: addListItem
   };
 })();
 
-/*
-//The code below runs a loop to list the names and heights of the pokemon
-// VSCode recommended using template literals instead of the quotes I was using
- 
-for (let i = 0; i < pokemonList.length; i++) {
-  //opening <p> tag, so the iteration is formatted in html
-  document.write('<p>');
- 
-  if (pokemonList[i].height <= 3)
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) `);
-  else {
-    document.write(`${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big! `);
-  }
-  //closing </p> tag so the iteration is html valid
-  document.write('</p>');
- 
-}*/
-
-//forEach function to run over pokemonList
-function getPokemonList(pokemon) {
-  document.write('<p>');
-  document.write(`${pokemon.name} (height: ${pokemon.height}) `);
-  if (pokemon.height > 3)
-    document.write(` -WOW that's big! `);
-  document.write('</p>');
-}
-//access getAll key to reach into the pokemonRepository IIFE, and get at pokemonList
-pokemonRepository.getAll().forEach(getPokemonList);
-
+//This forEach loop references within the IIFE andcalls the addListItem function
+pokemonRepository.getAll().forEach(function (pokemon) {
+  pokemonRepository.addListItem(pokemon);
+});
